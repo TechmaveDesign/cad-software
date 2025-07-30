94590e7dc03631634adc1ed3aeb53cb8da94ecdf
TechmaveDesign <designs@techmavesoftware.com>
2025-07-30 13:53:16 +0530

    Updated Viewport3D.tsx

--- src/components/Viewport3D.tsx
+++ src/components/Viewport3D.tsx
@@ -549,6 +549,12 @@ interface Viewport3DProps {
           {annotations.length > 0 && (
             <span className="ml-4 text-green-400">Annotations: {annotations.length}</span>
           )}
+          {activeTool === 'polygon' && polygonPoints.length > 0 && (
+            <span className="ml-4 text-yellow-400">Polygon Points: {polygonPoints.length} (Double-click to finish)</span>
+          )}
+          {activeTool === 'bezier' && bezierControlPoints.length > 0 && (
+            <span className="ml-4 text-purple-400">Bezier Points: {bezierControlPoints.length}/4</span>
+          )}
         </div>
       </div>
 
